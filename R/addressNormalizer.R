555a049d6e9685ce3937604bc9cb2a1a80494cee
Tajti András <atajti@gmail.com>
2017-05-12 14:37:14 +0200

    addressNormalizer nem akad fenn a többszörösen előforduló irszeken, csomagverzió növelve.

--- R/addressNormalizer.R
+++ R/addressNormalizer.R
@@ -5,6 +5,13 @@
 #'
 #' @param x tisztítandó címeket tartalmazó vektor
 #'
+#'
+#' @section TODO:
+#' A városok jelenleg egy lista alapján irányítoszámokból jön létre.
+#'   Mivel egy irányítoszám több elepüléshez tartozik, ez a módszer
+#'   adatvesztéssel jár. A listát városnév-tisztításra kéne csak
+#'   használni.
+#'
 #' @return
 #' karakter vektor, \code{x} minden elemére egy javított verzió
 #'
@@ -19,11 +26,13 @@
 
 addressNormalizer <- function(x){
   irszam <- cleanZip(x)
+  varosok <- varosok[which(!duplicated(varosok$postal)),]
   irsz_sorrend <- sapply(irszam,
                          function(irsz){
                            which(irsz == varosok$postal)
                          })
-  varos <- cleanCity(varosok[irsz_sorrend, "City"])
+  irsz_sorrend[(sapply(irsz_sorrend, length)==0)] <- NA # nem talált irszek NA lesznek
+  varos <- cleanCity(unname(unlist(varosok[unlist(irsz_sorrend), "City"])))
   street <- cleanStreet(x)
   return(paste(irszam,varos,street))
 }
