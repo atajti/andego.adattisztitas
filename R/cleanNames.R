3171aae55b71d1ae17ba564e5a9280931775b7bb
Tajti András <atajti@gmail.com>
2017-06-06 00:45:15 +0200

    cleanNames vektorizáltan eldönti, hol kell céget és hol kell személynevet tisztítani. Tesztek is készültek.

---
+++ R/cleanNames.R
@@ -0,0 +1,33 @@
+#' Névtisztítás
+#'
+#' Cég- és személynevek egységes alakra hozása
+#'
+#' @param x karakter vektor, megtisztítandó személynevekkel
+#'
+#' @details
+#' A függvény valójában csak egy wrapper, a \code{detectFirms} eredménye
+#'   alapján eldönti, melyik névre alkalmazza a \code{cleanPersonNames}
+#'   és melyikre a \code{cleanFirms} függvényt.
+#'
+#' @return
+#' karakter vektor, a tisztított nevekkel
+#'
+#' @author
+#' Tajti András <atajti@andego.hu>
+#' 
+#' @examples
+#' cleanNames("Sándor József Benedek")
+#' cleanNames("S. József Benedek")
+#' cleanNames("Sándor J. Benedek")
+#' cleanFirms(toupper(unAccent(c("Andego Tanácsadó Korlátolt felelősségű társaság",
+#'                               "Magyar Telekom Nyrt"))))
+#'
+#' @export
+
+cleanNames <- function(x){
+  res <- character(length(x))
+  firms <- detectFirms(x)
+  res[!firms] <- cleanPersonNames(x[!firms])
+  res[firms] <- cleanFirms(x[firms])
+  return(res)
+}
---
+++ tests/testthat/test_cleanNames.R
@@ -0,0 +1,18 @@
+library(andego.adattisztitas)
+context("cleanNames")
+
+test_that("cégek megtisztulnak", {
+    expect_equal(cleanNames(toupper(unAccent(
+               c("Andego Tanácsadó Korlátolt felelősségű társaság",
+                 "Magyar Telekom Nyrt")))),
+               c("ANDEGO TANACSADO KFT", "MAGYAR TELEKOM RT"))
+})
+
+test_that("magánszemélyek megtisztulnak", {
+  expect_identical(cleanNames("Sándor József"),
+                   "SANDOR JOZSEF")
+  expect_identical(cleanNames("Sándor József Benedek"),
+                   "SANDOR J BENEDEK")
+  expect_identical(cleanNames("S. József Benedek"),
+                   "S JOZSEF BENEDEK")
+})
