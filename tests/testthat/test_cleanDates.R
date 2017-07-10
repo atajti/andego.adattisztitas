fb7188236690ae8434a1b4262aa6358d31b2d4e7
Tajti András <atajti@gmail.com>
2017-07-10 10:19:14 +0200

    cleanDates és tesztek kész

---
+++ tests/testthat/test_cleanDates.R
@@ -0,0 +1,29 @@
+library(andego.adattisztitas)
+context("cleanDates")
+
+test_that("hosszok_egyeznek",{
+  expect_equal(length(cleanDates(character(0))),
+               0)
+  expect_equal(length(cleanDates("2017. 07. 10")),
+               1)
+  expect_equal(length(cleanDates(c("2017. 07. 10",
+                                    "2017. 07. 10"))),
+               2)
+  })
+
+test_that("NA-k a helyükön vannak", {
+  expect_equal(which(is.na(cleanDates(NA))),
+               1)
+  expect_equal(which(is.na(cleanDates(c("2017. 07. 10",
+                                         NA)))),
+               2)
+  expect_equal(which(is.na(cleanDates(c(NA,
+                                        "2017. 07. 10",
+                                         NA)))),
+               c(1, 3))
+  })
+
+test_that("működik a példa", {
+  expect_equal(cleanDates("56-10-23"),
+               as.Date("1956-10-23"))
+  })
